020b6c8e5d55484b1d1334dfcc7b38ea1b0d6d69
Idan Regev <idanre1@gmail.com>
2021-02-03 11:32:35 +0200

    zsh final

--- install_zsh.sh
+++ install_zsh.sh
@@ -2,10 +2,11 @@
 aptyes='sudo DEBIAN_FRONTEND=noninteractive apt-get -y '
 
 # Install shell
-aptyes zsh
+$aptyes install zsh
 
 # attended install
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Don't run zsh at the end, since more installations are needed afterwards
+env RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # zsh Plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
