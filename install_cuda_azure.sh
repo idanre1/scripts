c895d86140d737a93f15bfc79a59c523f686bdf7
Idan Regev <idanre1@gmail.com>
2021-10-20 17:33:10 +0300

    rapids v21

--- install_cuda_azure.sh
+++ install_cuda_azure.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 # https://docs.microsoft.com/en-us/azure/virtual-machines/linux/n-series-driver-setup
 # https://developer.download.nvidia.com/compute/cuda/repos/
+# for non azure vm:
+# lspci
+# sudo lshw -C Display
 aptyes='sudo DEBIAN_FRONTEND=noninteractive apt-get -y '
 
 # Make sure GPU is in
 lspci | grep -i NVIDIA
 
-# From template
-CUDA_REPO_PKG=cuda-repo-ubuntu1804_10.2.89-1_amd64.deb
-wget -O /tmp/${CUDA_REPO_PKG} https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/${CUDA_REPO_PKG} 
+# Manual install
+# https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=20.04&target_type=runfile_local
+# don't forget NOT to install the driver when asked , only the toolkit
+# wget -O cuda-driver.run https://developer.download.nvidia.com/compute/cuda/11.4.2/local_installers/cuda_11.4.2_470.57.02_linux.run
+# sudo sh cuda-driver.run
+# rm -f cuda-driver.run
+# CUDAVER=`ls -1 /usr/local| grep cuda- | head -1`
+# CUDALIB="/usr/local/$CUDAVER/lib64"
+# sudo sh -c "echo include $CUDALIB >> /etc/ld.so.conf"
+# sudo ldconfig
 
-sudo dpkg -i /tmp/${CUDA_REPO_PKG}
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub 
-rm -f /tmp/${CUDA_REPO_PKG}
-
+# apt install
 $aptyes update
-$aptyes install cuda-drivers
+$aptyes install nvidia-cuda-toolkit
 
 # default pyhton env init
 source ~/settings/python_init.sh
--- install_python_cuda.sh
+++ install_python_cuda.sh
@@ -11,7 +11,7 @@ rm ~/$CONDA_FILE
 # Create cuda env
 source ~/miniconda3/etc/profile.d/conda.sh
 # https://rapids.ai/start.html#rapids-release-selector
-conda create -n rapids-0.18 -c rapidsai -c nvidia -c conda-forge \
-    -c defaults rapids=0.18 python=3.8 cudatoolkit=11.0
+conda create -n rapids-21.10 -c rapidsai -c nvidia -c conda-forge \
+    rapids-blazing=21.10 python=3.8 cudatoolkit=11.2
 
 #https://medium.com/rapids-ai/plotly-census-viz-dashboard-powered-by-rapids-1503b3506652
