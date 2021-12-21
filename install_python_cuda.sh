4dab3e6eb12d3f1392ef0f44d9a9b7821888669e
Idan Regev <idanre1@gmail.com>
2021-12-21 13:57:39 +0200

    pytorch

--- install_python_cuda.sh
+++ install_python_cuda.sh
@@ -1,35 +1,42 @@
 #!/bin/bash
 # ***source the file*** for install correctly
-# Fresh miniconda
+CUDA_VER=11.4
+ENV_NAME=cuda_$CUDA_VER
+RAPIDS_VER=21.12
+
 cd ~
-CONDA_FILE=miniconda.sh
-#wget https://repo.anaconda.com/miniconda/Miniconda3-4.5.4-Linux-x86_64.sh $CONDA_FILE
-wget -O $CONDA_FILE https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-chmod +x $CONDA_FILE
-./$CONDA_FILE -b
-rm ~/$CONDA_FILE
+
+if [ ! -f "/home/$USER/miniconda3/etc/profile.d/conda.sh" ]; then
+    echo "Installing Fresh miniconda"
+    CONDA_FILE=miniconda.sh
+    #wget https://repo.anaconda.com/miniconda/Miniconda3-4.5.4-Linux-x86_64.sh $CONDA_FILE
+    wget -O $CONDA_FILE https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    chmod +x $CONDA_FILE
+    ./$CONDA_FILE -b
+    rm ~/$CONDA_FILE
+fi
 
 # Create cuda env
+echo "Building env"
 source ~/miniconda3/etc/profile.d/conda.sh
-PYVER=rapids-21.10
-CUDA_VER=11.2
 # https://rapids.ai/start.html#rapids-release-selector
-conda create -n $PYVER -c rapidsai -c nvidia -c conda-forge \
-    rapids-blazing=21.10 python=3.8 cudatoolkit=$CUDA_VER -y
+conda create -n $ENV_NAME -c rapidsai -c nvidia -c conda-forge \
+    rapids=$RAPIDS_VER python=3.8 cudatoolkit=$CUDA_VER dask-sql -y
 
 #https://medium.com/rapids-ai/plotly-census-viz-dashboard-powered-by-rapids-1503b3506652
 
 # site libs
-ln -s /nas/python_lib /nas/miniconda3/envs/$PYVER/lib/python3.8/site-packages/site-packages.pth
+ln -s /nas/python_lib /nas/miniconda3/envs/$ENV_NAME/lib/python3.8/site-packages/site-packages.pth
 
 
 # Add own stuff
-conda activate $PYVER
+conda activate $ENV_NAME
 conda install -c conda-forge mamba -y # installs much faster than conda
 
-# aptyes='sudo DEBIAN_FRONTEND=noninteractive apt-get -y '
 #might not needed ? $aptyes install azure-cli 
 mamba install -c conda-forge dvc dvc-azure chardet -y
-mamba install -c conda-forge  pyAesCrypt -y
-mamba install -c conda-forge  seaborn missingno mplfinance -y
-mamba install -c pytorch pytorch torchvision torchaudio cudatoolkit=$CUDA_VER -y
+mamba install -c conda-forge pyAesCrypt -y
+mamba install -c conda-forge seaborn missingno mplfinance -y
+
+# pytorch from docker
+# tar -xf /datadrive/docker_images/pytorch.tar.gz -C /nas/miniconda3/envs/$ENV_NAME/lib/python3.8/site-packages/
--- install_python_datascience.sh
+++ install_python_datascience.sh
@@ -7,25 +7,13 @@ pip -V
 
 #packages
 pip install missingno
-pip install plotly
+pip install plotly kaleido # kaleido is for static images render
 pip install tables #hd5 for pandas
 pip install pyarrow #parquet
 pip install datashader holoviews # bigdata scatter plots
 pip install PyWavelets
 
-# orca for plotly
-# cd ~
-# ORCA_FILE=orca-1.3.1.AppImage
-# wget https://github.com/plotly/orca/releases/download/v1.3.1/$ORCA_FILE
-# chmod +x orca-*.AppImage
-# sudo apt-get install xvfb
-
-# echo '#!/bin/bash' >> /nas/py3env/bin/orca
-# echo "xvfb-run -a /nas/$ORCA_FILE \"\$@\"" >> /nas/py3env/bin/orca
-# chmod +x /nas/py3env/bin/orca
-
-# HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP
-# How to use in plotly
-# plotly.io.orca.config.executable = '/nas/py3env/bin/orca'
-# plotly.io.renderers.default = "svg"
-# HELP HELP HELP HELP HELP HELP HELP HELP HELP HELP
+# pytorch
+pip install torch==1.10.1+cu113 torchvision==0.11.2+cu113 torchaudio==0.10.1+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
+# fastai
+pip install fastai
