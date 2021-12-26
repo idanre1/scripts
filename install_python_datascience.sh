789abd71df29b5868cf8ac72ec6fc0b94f95ee68
Idan Regev <idanre1@gmail.com>
2021-12-26 20:59:01 +0200

    dvc

--- install_python_datascience.sh
+++ install_python_datascience.sh
@@ -12,6 +12,7 @@ pip install tables #hd5 for pandas
 pip install pyarrow #parquet
 pip install datashader holoviews # bigdata scatter plots
 pip install PyWavelets
+pip install "dvc[all]"
 
 PYVER=`ls -1 ~/Envs/py3env/lib/ | grep "python" | head -1`
 ln -s /nas/settings/site-packages.pth /nas/Envs/py3env/lib/$PYVER/site-packages/site-packages.pth
