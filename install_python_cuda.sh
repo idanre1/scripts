5f4f1c73b28925d7c134b4a520a21e1e395bbcdd
Idan Regev <idanre1@gmail.com>
2023-01-24 09:36:32 +0200

    all gpu libs on same python cuda

--- install_python_cuda.sh
+++ install_python_cuda.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # ***source the file*** for install correctly
-CUDA_VER=11.4
+# https://rapids.ai/start.html#get-rapids
+# https://pytorch.org/get-started/locally/
+# Select upper cuda version
+PYTHON_VER=3.9
+RAPIDS_VER=22.12
+CUDA_VER=11.7
 ENV_NAME=cuda_$CUDA_VER
-RAPIDS_VER=21.12
 
 cd ~
 
@@ -19,24 +23,22 @@ fi
 # Create cuda env
 echo "Building env"
 source ~/miniconda3/etc/profile.d/conda.sh
-# https://rapids.ai/start.html#rapids-release-selector
-conda create -n $ENV_NAME -c rapidsai -c nvidia -c conda-forge \
-    rapids=$RAPIDS_VER python=3.8 cudatoolkit=$CUDA_VER dask-sql -y
+# mamba installs much faster than conda
+conda create -n $ENV_NAME -c rapidsai -c conda-forge -c nvidia \
+    python=$PYTHON_VER mamba -y
+echo "Building env...done"
 
 #https://medium.com/rapids-ai/plotly-census-viz-dashboard-powered-by-rapids-1503b3506652
 
 # site libs
-ln -s /nas/settings/site-packages.pth /nas/miniconda3/envs/$ENV_NAME/lib/python3.8/site-packages/site-packages.pth
+ln -s /nas/settings/site-packages.pth /nas/miniconda3/envs/$ENV_NAME/lib/python${PYTHON_VER}/site-packages/site-packages.pth
 
 
 # Add own stuff
 conda activate $ENV_NAME
-conda install -c conda-forge mamba -y # installs much faster than conda
-
-#might not needed ? $aptyes install azure-cli 
-mamba install -c conda-forge dvc dvc-azure chardet -y
-mamba install -c conda-forge pyAesCrypt -y
+# Both rapids and pytorch on same env
+mamba install -c rapidsai -c pytorch -c conda-forge -c nvidia rapids=$RAPIDS_VER cudatoolkit=$CUDA_VER pytorch torchvision torchaudio pytorch-cuda=$CUDA_VER -y
+mamba install -c conda-forge dask-sql -y
+mamba install -c fastchan -c conda-forge -c fastai -c timeseriesai fastai tsai fastkaggle gpustat -y
+mamba install -c conda-forge dvc dvc-azure chardet pyAesCrypt -y
 mamba install -c conda-forge seaborn missingno mplfinance -y
-
-# pytorch from docker
-# tar -xf /datadrive/docker_images/pytorch.tar.gz -C /nas/miniconda3/envs/$ENV_NAME/lib/python3.8/site-packages/
