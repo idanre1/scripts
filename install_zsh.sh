7bdad63e7e272adb6d881d87bf42e91c7028e99c
Idan Regev <idanre1@gmail.com>
2023-02-02 10:01:18 +0200

    keep vanilla

--- install_zsh.sh
+++ install_zsh.sh
@@ -14,7 +14,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Add flavors
-# KEEP_ZSHRC mv ~/.zshrc ~/.zshrc.vanilla
+mv ~/.zshrc ~/.zshrc.vanilla
 ln -s ~/settings/zshrc ~/.zshrc
 
 # Making default shell
