0ec12ca1953ff398b0cd1d79a97008f4fcce9b86
Idan Regev <idanre1@gmail.com>
2026-02-10 09:48:04 +0200

    fzf

--- install_droplet.sh
+++ install_droplet.sh
@@ -1,6 +1,41 @@
 #!/bin/bash
-# Usage: 
-#wget https://raw.githubusercontent.com/idanre1/scripts/master/install_droplet.sh
+# Usage:
+# wget https://raw.githubusercontent.com/idanre1/scripts/master/install_droplet.sh
+# bash install_droplet.sh [--extended] [--zsh]
+
+extended=0
+setup_zsh=0
+
+usage() {
+	echo "Usage: $0 [--extended] [--zsh]"
+}
+
+add_line_if_missing() {
+	local line="$1"
+	local file="$2"
+	grep -Fqx "$line" "$file" 2>/dev/null || echo "$line" >> "$file"
+}
+
+while [[ $# -gt 0 ]]; do
+	case "$1" in
+		--extended)
+			extended=1
+			;;
+		--zsh)
+			setup_zsh=1
+			;;
+		-h|--help)
+			usage
+			exit 0
+			;;
+		*)
+			echo "Unknown option: $1"
+			usage
+			exit 1
+			;;
+	esac
+	shift
+done
 cd ~
 aptyes='sudo DEBIAN_FRONTEND=noninteractive apt-get -y '
 
@@ -40,3 +75,16 @@ cd $py3bin
 source activate
 python --version
 deactivate
+
+# -----------------------------------------
+# Extended installation
+# -----------------------------------------
+if [[ $extended -eq 1 ]]; then
+    echo "*** Extended installation ***"
+	$aptyes install btop jq unzip
+fi
+
+if [[ $setup_zsh -eq 1 ]]; then
+    echo "*** Setting up zsh ***"
+	source ~/scripts/install_zsh.sh
+fi
--- install_zsh.sh
+++ install_zsh.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 aptyes='sudo DEBIAN_FRONTEND=noninteractive apt-get -y '
+$aptyes update
 
 # Install shell
-$aptyes install zsh
+$aptyes install zsh fzf
 
 # attended install
 # Don't run zsh at the end, since more installations are needed afterwards
@@ -12,7 +13,8 @@ env RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.c
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-git clone --depth=1 https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+# git clone --depth=1 https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+git clone --depth=1 https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 # Add flavors
